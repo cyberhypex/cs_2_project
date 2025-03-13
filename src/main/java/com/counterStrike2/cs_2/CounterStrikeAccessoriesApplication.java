92f3aa7b53e5e86b4645a25a18665fc541f52840
Anshuman <chaoanxuman999@gmail.com>
2025-03-13 15:04:08 +0530

    Making the backend for my new Project

---
+++ src/main/java/com/counterStrike2/cs_2/CounterStrikeAccessoriesApplication.java
@@ -0,0 +1,13 @@
+package com.counterStrike2.cs_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterStrikeAccessoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterStrikeAccessoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/counterStrike2/cs_2/CounterStrikeAccessoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counterStrike2.cs_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterStrikeAccessoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
